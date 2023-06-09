4dad819bb09dd6e5581109b144f21f26e134ebaa
akhileshgowda <akhileshgowda7@gmail.com>
2023-06-08 20:49:55 -0400

    Added and created get cities

--- cityInfo.API/Program.cs
+++ cityInfo.API/Program.cs
@@ -3,7 +3,7 @@
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -13,14 +13,22 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
+
     app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
 
+app.UseRouting();
 app.UseAuthorization();
 
-app.MapControllers();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+
+
 
 app.Run();
 
